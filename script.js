f64debd7b5ba36b4f9ed295c9d17a3a485f0333c
Purvik <kotechapurvik123@gmail.com>
2023-06-29 14:50:20 +0530

    commit

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
 
+const express = require("express");
+const session = require('express-session');
+const https = require('https');
+const { stringify } = require("querystring");
+const cors = require('cors');
+var path = require('path');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+app.use(session({secret: 'ssshhhhh',saveUninitialized: true,resave: true}));
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(cors());
+app.use(express.static(path.join(__dirname, "js")));
+
+let sess;
+app.get('/', (req, res) => {
+  sess = req.session;
+  sess.order_id;
+  res.sendFile(__dirname +  '/index.html');
+});
 /*
  * Define the version of the Google Pay API referenced when creating your
  * configuration
@@ -159,3 +182,5 @@ const baseRequest = {
     }
   }
     
+
+  app.listen(PORT, () => console.log(`Server Started on ${PORT}`));
